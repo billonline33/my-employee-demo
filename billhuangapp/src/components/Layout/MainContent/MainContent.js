c4186198ddf7532d5a08e0341fa886aa6603b487
billonline33 <billonline33@hotmail.com>
2018-07-08 22:49:27 +1000

    refactor(filter section): Merge SortBy componet into MainContent
    combine SortBy and MainContent into one component, to simplify code when pass state.
    
    I did not reuse redux for the exercise, but I will use if this is a real project

--- billhuangapp/src/components/Layout/MainContent/MainContent.js
+++ billhuangapp/src/components/Layout/MainContent/MainContent.js
@@ -1,17 +1,36 @@
 import React, { Component } from "react";
-import SortBy from "../../Employee/SortBy";
 import Card from "../../Employee/Card";
 import { Col, Row } from "react-bootstrap";
 import { FormControl } from "react-bootstrap";
 import "./MainContent.css";
+import axios from "axios";
+import { SplitButton, MenuItem } from "react-bootstrap";
 
 class MainContent extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: ""
+      value: "",
+      companyInfo: {},
+      employeeInfo: [],
+      sortByField: "firstName"
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSortByChange = this.handleSortByChange.bind(this);
+  }
+
+  getCompanyInfo() {
+    axios.get("/api/companyInfo").then(response => {
+      console.log("comp Info: ", response.data);
+      this.setState({ companyInfo: response.data });
+    });
+  }
+
+  getEmployeeInfo() {
+    axios.get("/api/employees").then(response => {
+      console.log("employees Info: ", response.data);
+      this.setState({ employeeInfo: response.data });
+    });
   }
 
   handleChange(e) {
@@ -19,12 +38,29 @@ class MainContent extends Component {
     console.log("search text=", this.state.value);
   }
 
+  handleSortByChange = sortByField => {
+    this.setState({ sortByField }, function() {
+      console.log("this.state.sortByFiled=", this.state.sortByField);
+    });
+  };
+
+  componentDidMount() {
+    console.log("componentDidMount Run");
+    this.getCompanyInfo();
+    this.getEmployeeInfo();
+  }
+
   render() {
     var paddingTop = {
       "padding-top": "13px"
     };
 
-    const { employeeInfo } = this.props;
+    const paddingStyle = {
+      padding: "8px",
+      float: "left"
+    };
+
+    const { sortByField } = this.state;
 
     return (
       <div className={"content-area"}>
@@ -33,7 +69,38 @@ class MainContent extends Component {
         </div>
         <div className={"floatRight"}>
           <div className={"floatLeft"}>
-            <SortBy />
+            <div className={"floatLeft labelStyle"}> Sort by:</div>
+            <div className={"floatLeft"} style={paddingStyle}>
+              <SplitButton bsSize="large" title={`${sortByField} `}>
+                <MenuItem
+                  eventKey="firstName"
+                  onSelect={this.handleSortByChange}
+                >
+                  First Name
+                </MenuItem>
+                <MenuItem
+                  eventKey="lastName"
+                  onSelect={this.handleSortByChange}
+                >
+                  Last Name
+                </MenuItem>
+                <MenuItem
+                  eventKey="jobTitle"
+                  onSelect={this.handleSortByChange}
+                >
+                  jobTitle
+                </MenuItem>
+                <MenuItem eventKey="age" onSelect={this.handleSortByChange}>
+                  age
+                </MenuItem>
+                <MenuItem
+                  eventKey="dateJoined"
+                  onSelect={this.handleSortByChange}
+                >
+                  dateJoined
+                </MenuItem>
+              </SplitButton>
+            </div>
           </div>
           <div className={"floatLeft labelStyle paddingTop"}>Search: </div>
           <div className={"floatLeft"} style={paddingTop}>
@@ -47,7 +114,7 @@ class MainContent extends Component {
         </div>
         <Row>
           <Col xs={12}>
-            {employeeInfo.map((item, index) => {
+            {this.state.employeeInfo.map((item, index) => {
               return <Card employeeInfo={item} key={item.id} />;
             })}
           </Col>
